9384d78437b6519f27a98816745f3e4faf3630fa
Sudin Kominfotik JB 04 <graficajati339@gmail.com>
2025-11-04 22:01:46 +0700

    membuat bulatan di tengah

--- lib/screen/splash_screen_1.dart
+++ lib/screen/splash_screen_1.dart
@@ -6,18 +6,21 @@ class MySplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(height: 50),
-          Container(
-            width: 250,
-            height: 250,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.redAccent
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 50),
+            Container(
+              width: 250,
+              height: 250,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.redAccent
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
