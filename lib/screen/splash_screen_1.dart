bfa401f2904e8b66ff7ab48f8a4e2ba555974aaa
Sudin Kominfotik JB 04 <graficajati339@gmail.com>
2025-11-04 21:55:29 +0700

    Update menambahkan bodyl

--- lib/screen/splash_screen_1.dart
+++ lib/screen/splash_screen_1.dart
@@ -5,6 +5,20 @@ class MyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(height: 50),
+          Container(
+            width: 250,
+            height: 250,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.redAccent
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
