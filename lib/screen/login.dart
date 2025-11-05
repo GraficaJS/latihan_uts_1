7a5538a55617dc4e08477b3c7a9e49501e9e5726
Sudin Kominfotik JB 04 <graficajati339@gmail.com>
2025-11-05 22:24:27 +0700

    Menambahkan tulisan dan gambar Email di login.dart

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -6,19 +6,42 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(  
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(20.0),
-          child: const Column(
-            children: [
-              SizedBox(height: 10),
-              Icon(
-                Icons.lock_outline,
-                size: 80,
-                color: Colors.green,
-              ),
-              SizedBox(height: 10),
-            ],
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                SizedBox(height: 10),
+                Icon(
+                  Icons.lock_outline,
+                  size: 80,
+                  color: Colors.green,
+                ),
+                SizedBox(height: 10),
+                Text("Welcome",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),),
+                SizedBox(height: 10),
+                Text("Silahkan Login",
+                style: TextStyle(
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.normal,
+                ),),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    hintText: "Masukan email anda",
+                    prefixIcon: Icon(Icons.email_outlined),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular (12.0),
+                    )),
+                ),
+              ],
+            ),
           ),
         ),
       ),
